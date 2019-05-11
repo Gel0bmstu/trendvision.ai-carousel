c7cc9c3110d256556a79076b99658a781ae9f743
Gel0bmstu <leshikne@gmail.com>
2019-05-11 16:33:10 +0300

    logo changed

--- static/js/components/headerComponent/header.js
+++ static/js/components/headerComponent/header.js
@@ -1,6 +1,6 @@
 const templateScript = `
     <div class="header__container">
-        <img src="./icons/S7_new_logo.png" class="header__container-logo" alt="">
+        <img src="./icons/trendvisionLogo.png" class="header__container-logo" alt="">
         <div class="header__container-hrefs-block">
             <a class="header__container-hrefs-block-statistic"><i class="fas fa-chart-bar"></i> Statistic</a>
             <a class="header__container-hrefs-block-statistic"><i class="fas fa-cog"></i> Settings</a>
