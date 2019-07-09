2245c8e98402f5e6dc3533016c16be2fbcb6d159
Gel0bmstu <leshikne@gmail.com>
2019-07-10 00:46:35 +0300

    # settings name change

--- static/js/components/settingsComponent/settings.js
+++ static/js/components/settingsComponent/settings.js
@@ -12,11 +12,13 @@ export default function getTemplate(admin) {
                     <input type="radio" class="settings__inputs-section-input markup" name="mode">Markup </br>
                 </form>
                 <hr class="settings__inputs-section-hr">
-                <p class="settings__inputs-section-tip">Show only currency</p>
+                <p class="settings__inputs-section-tip">Show only global graphics</p>
                 <input type="checkbox" class="settings__inputs-section-input currency">
-                <p class="settings__inputs-section-tip">Show only gems</p>
+                <p class="settings__inputs-section-tip">Show only global shifts</p>
+                <input type="checkbox" class="settings__inputs-section-input currency">
+                <p class="settings__inputs-section-tip">Show only trends</p>
                 <input type="checkbox" class="settings__inputs-section-input gems">
-                <p class="settings__inputs-section-tip">Show only companies</p>        
+                <p class="settings__inputs-section-tip">Show only industries</p>        
                 <input type="checkbox" class="settings__inputs-section-input companies">
             </div>
             <button class="settings-apply-btn" data-section="save"><i class="fas fa-save" data-section="save"></i> Save</button>
@@ -26,11 +28,13 @@ export default function getTemplate(admin) {
             <i class="fas fa-chevron-left settings-close"></i>
             <p class="settings-title">Settings</p>
             <div class="settings__inputs-section">
-                <p class="settings__inputs-section-tip">Show only currency</p>
+                <p class="settings__inputs-section-tip">Show only global graphics</p>
                 <input type="checkbox" class="settings__inputs-section-input currency">
-                <p class="settings__inputs-section-tip">Show only gems</p>
+                <p class="settings__inputs-section-tip">Show only global shifts</p>
+                <input type="checkbox" class="settings__inputs-section-input currency">
+                <p class="settings__inputs-section-tip">Show only trends</p>
                 <input type="checkbox" class="settings__inputs-section-input gems">
-                <p class="settings__inputs-section-tip">Show only companies</p>        
+                <p class="settings__inputs-section-tip">Show only industries</p>        
                 <input type="checkbox" class="settings__inputs-section-input companies">
             </div>
             <button class="settings-apply-btn" data-section="save"><i class="fas fa-save" data-section="save"></i> Save</button>
